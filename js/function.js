6afdf7519b66bf72d74d53ad44009cec76c90865
Vaki1995 <nvkien170395@gmail.com>
2018-04-14 01:19:03 +0700

    Hoàn thành css, html trang index

--- js/function.js
+++ js/function.js
@@ -1,4 +1,5 @@
 var product = [
+	//Asus
 		{
 			"type":"corei7",
 			"producer":"Asus",
@@ -27,14 +28,147 @@ var product = [
 			"name":"Laptop Asus ROG-Strix-Hero GL503VD-GZ119T Core i7-7700HQ",
 			"price":24209000,
 			"img":"Asus-ROG-Strix-Hero-GL503VD-GZ119T-Corei7-7700HQ.jpg",
-			"info":["CPU	Intel Core i7 7700HQ 2.8GHz up to 3.8GHz 6MB","RAM	8GB DDR4 2400MHz","Đĩa cứng	SSHD 1TB 5400rpm (8GB Cache)","Card đồ họa	NVIDIA GeForce GTX 1050 4GB GDDR5 + Intel Graphics 630","Màn hình 15.6 Inches FHD (1920 x 1080) IPS Non-Glare, 100% sRGB, 120Hz, 300nits"],
+			"info":["CPU Intel Core i7 7700HQ 2.8GHz up to 3.8GHz 6MB","RAM	8GB DDR4 2400MHz","Đĩa cứng	SSHD 1TB 5400rpm (8GB Cache)","Card đồ họa NVIDIA GeForce GTX 1050 4GB GDDR5 + Intel Graphics 630","Màn hình 15.6 Inches FHD (1920 x 1080) IPS Non-Glare, 100% sRGB, 120Hz, 300nits"],
 			"numSee":0,
 			"numSale":0,
 			"madeIn":"Đài Loan"
 		}, //3
+		{
+			"type":"corei5",
+			"producer":"Asus",
+			"name":"Laptop Asus FX503VM-E4087T Core i5-7300HQ",
+			"price":26069000,
+			"img":"Asus-FX503VM-E4087T-Corei5-7300HQ.jpg",
+			"info":["Chip: Intel Core i5-7300HQ","RAM: 8GB DDR4 2400MHz","Ổ cứng: 1TB SSHD 5400rpm","Chipset đồ họa: NVIDIA GeForce GTX1060 6GB GDDR5","Màn hình: 15.6 inch Full HD (1920 x 1080) IPS, 60Hz Anti-Glare Panel with 45% NTSC","Hệ điều hành: Windows 10 Home","Pin: 4 Cells 64 Whrs"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //4
+		{
+			"type":"corei5",
+			"producer":"Asus",
+			"name":"Laptop Asus X510UQ-BR632 Core i5-8250U",
+			"price":13549000,
+			"img":"Asus-X510UQ-BR632-Corei5-8250U.jpg",
+			"info":["CPU: Intel Core i5-8250U 1.6GHz up to 3.4GHz 6MB","RAM: 4GB DDR4 2400MHz","Ổ cứng: HDD: 1 TB","Màn hình	15.6 inch HD (1366 x 768) Anti-Glare","Đĩa cứng	HDD 1TB 5400rpm, x1 slot SSD M.2 SATA","Card đồ họa: NVIDIA GeForce GT 940MX 2GB GDDR5 + Intel UHD Graphics 620","Loại PIN: PIN liền, thông tin Pin: Li-Ion 3 cell","Hệ điều hành: Windows 10"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //5
+		{
+			"type":"corei5",
+			"producer":"Asus",
+			"name":"Laptop Asus Zenbook UX430UA-GV334T Core i5-8250U",
+			"price":14490000,
+			"img":"Asus-Zenbook-UX430UA-GV334T-Corei5-8250U.jpg",
+			"info":["Chip: Intel Core i5-8250U (1.6GHz Up to 3.4GHz, 6MB Cache)","RAM: 8GB DDR4-2400MHz","Ổ cứng: 256GB (M.2/SATA)","Chipset đồ họa: Intel UHD Graphics 620","Màn hình: 14 inch FHD (1920 x 1080) with wide 100% sRGB color gamut","Hệ điều hành: Windows 10 Home - 64bit","Pin: 3 Cell 50 Whrs"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //6
+		{
+			"type":"corei3",
+			"producer":"Asus",
+			"name":"Laptop Asus Vivobook X411UA-BV221T Core i3-7100U",
+			"price":10359000,
+			"img":"Asus-Vivobook-X411UA-BV221T-Corei3-7100U.jpg",
+			"info":["Chip: Intel Core i3-7100U","RAM: 4GB DDR4 2133MHz","Ổ cứng: HDD: 1 TỔ cứng: 1TB HDD 5400rpm + 1 slot SSD M.2 Sata3B","Chipset đồ họa: Intel HD Graphics 620","Màn hình: 14 inch HD (1366 x 768) LED Backlit Anti-Glare Panel with 45% NTSC","Hệ điều hành: Windows 10 Home","Pin: 3 Cell 42 Whrs"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //7
+		{
+			"type":"corei3",
+			"producer":"Asus",
+			"name":"Laptop Asus Vivobook X510UA-BR650T Core i3-7100U",
+			"price":10969000,
+			"img":"Asus-Vivobook-X510UA-BR650T-Corei3-7100U.jpg",
+			"info":["Chip: Intel Core i3-7100U","RAM: 4GB DDR4 2133MHz (1x4GB) + 1 slot RAM","Ổ cứng: 1TB HDD 5400rpm + 1 slot SSD M.2 Sata3","Chipset đồ họa: Intel HD Graphics 620","Màn hình: 15.6 inch (16:9) LED backlit HD (1366 x 768) 60Hz Anti-Glare Panel with 45% NTSC","Hệ điều hành: Windows 10 Home","Pin: 3 Cell 42 Whrs"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //8
+		{
+			"type":"celoron",
+			"producer":"Asus",
+			"name":"Laptop Asus E402SA-WX043D Celeron-N3050",
+			"price":4369000,
+			"img":"Asus-E402SA-WX043D-Celeron-N3050.jpg",
+			"info":["Chip: Intel Celeron N3050","RAM: 2GB","Ổ cứng: 500GB","Chipset đồ họa: Intel HD Graphics","Màn hình: 14 Inches","Hệ điều hành: Free Dos","Pin: 2 Cells"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //9
+		{
+			"type":"pentium",
+			"producer":"Asus",
+			"name":"Laptop Asus E402SA-WX076D Pentium-N3700",
+			"price":5559000,
+			"img":"Asus-E402SA-WX076D-Pentium-N3700.jpg",
+			"info":["Chip: Intel Pentium™ N3700 1.6GHz","RAM: 4GB","Ổ cứng: 500GB","Chipset đồ họa: Intel HD Graphics","Màn hình: 14 Inches","Hệ điều hành: Dos","Pin: 2 Cells"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Đài Loan"
+		}, //10
+	//Dell
+		{
+			"type":"corei7",
+			"producer":"dell",
+			"name":"Laptop DELL Alienware 17-R4-70133743 Core i7-6700HQ",
+			"price":44990000,
+			"img":"DELL-Alienware-17-R4-70133743-Corei7-6700HQ.jpg",
+			"info":["Thiết kế sang trọng, mỏng nhẹ, tản nhiệt hiệu quả","Bộ vi xử lý Intel® Core™ i7-6700HQ Processor","Bộ nhớ RAM: 16GB DDR4 2667MHz","Ổ cứng: 256GB PCIe SSD + 1TB 7200RPM SATA","Màn hình 17.3 inch FHD (1920 x 1080) IPS Anti-Glare 300-nits Display","Hệ điều hành Windows 10 Home SL"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Nhật Bản"
+		}, //11
+		{
+			"type":"corei7",
+			"producer":"dell",
+			"name":"Laptop DELL Inspiron 7577–N7577A Core i7-7700HQ",
+			"price":27390000,
+			"img":"DELL-Inspiron-7577–N7577ACorei7-7700HQ.jpg",
+			"info":["Thiết kế mỏng nhẹ, sang trọng, chịu lực tốt","Bộ vi xử lý: Intel® Core™ i7-7700HQ Processor (6M Cache, 2.8 up to 3.80 GHz)","RAM: 8GB DDR4 2400MHz SDRAM","Ổ cứng: 1TB SATA 5400rpm + 128GB SSD","Màn hình: 15.6 inch độ phân giải full HD (1920 x 1080)","Card đồ họa: NVIDIA® GeForce® GTX 1050Ti with 4GB DDR5","Hệ điều hành: Windows 10"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Nhật Bản"
+		}, //12
+		{
+			"type":"corei7",
+			"producer":"dell",
+			"name":"Laptop Dell Inspiron 13-7370-7D61Y2 Core i7-8550U",
+			"price":31790000,
+			"img":"Dell-Inspiron-13-7370-7D61Y2-Corei7-8550U.jpg",
+			"info":["Chip: Intel Core i7-8550U","RAM: 16GB DDR4 2400MHz","Ổ cứng: 512GB SSD","Chipset đồ họa: Intel UHD Graphics 620","Màn hình: 13.3 inch FHD (1920 x 1080) IPS Truelife LED-Backlit Narrow Border","Hệ điều hành: Windows 10 home","Pin: 3 Cell 38 Whrs"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Nhật Bản"
+		}, //13
+		{
+			"type":"corei7",
+			"producer":"dell",
+			"name":"Laptop Dell XPS 15-9560-70123080 Core i7-7700HQ",
+			"price":48589000,
+			"img":"Dell-XPS-15-9560-70123080-Corei7-7700HQ.jpg",
+			"info":["Chip: Intel Core i7-7700HQ","RAM: 16GB DDR4 2400MHz, Max 32GB, 2 Slots","Ổ cứng: 512GB SSD M2 PCIe3x4","Chipset đồ họa: NVIDIA® GeForce® GTX 1050 with 4GB GDDR5 + Intel® HD Graphics 630","Màn hình: 15.6 inch 4K Ultra HD (3840 x 2160) InfinityEdge touch display","Hệ điều hành: Windows 10 bản quyền + Microsoft Office 365"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Nhật Bản"
+		}, //14
+		{
+			"type":"corei7",
+			"producer":"dell",
+			"name":"Laptop DEL Inspiron7460 N4I5259W Core i5-7200U",
+			"price":18049000,
+			"img":"DEL-Inspiron7460-N4I5259WCorei5-7200U.jpg",
+			"info":["Bộ vi xử lý: Intel® Core™ i5-7200U Processor","RAM: 4GB DDR4","HDD: 128 SSD + 500GB HDD","Card màn hình: Geforce 940MX DDR5 2GB","Màn hình: 14 inch Full HD","Hệ điều hành: Windows 10","Pin: 3 Cell Lithium-ion"],
+			"numSee":0,
+			"numSale":0,
+			"madeIn":"Nhật Bản"
+		}, //15
 	]
-$(document).ready(function(){
-	$("button").click(function(){
-		$("h1").append(product[0].info[1]);
-	});	
-});
+// $(document).ready(function(){
+// 	$("button").click(function(){
+// 		$("h1").append(product[0].info[1]);
+// 	});	
+// });
